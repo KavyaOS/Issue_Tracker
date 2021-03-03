c3eabfafad8def3620c5b1dfc28e2e2ee4dcbe9a
KavyaOS <kavya.os15@gmail.com>
2021-03-03 00:51:59 -0800

    Updated server.js to use Express and GraphQL

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
-const initialIssues = [  
-  {    
-    id: 1, status: 'New', owner: 'Ravan', effort: 5,    
-    created: new Date('2018-08-15'), due: undefined,    
-    title: 'Error in console when clicking Add',  
-  },
+const dateRegex = new RegExp('^\\d\\d\\d\\d-\\d\\d-\\d\\d');
 
-  {    
-    id: 2, status: 'Assigned', owner: 'Eddie', effort: 14,    
-    created: new Date('2018-08-16'), due: new Date('2018-08-30'),    
-    title: 'Missing bottom border on panel',  
-  },
-];
-
+function jsonDateReviver(key, value) {
+  if (dateRegex.test(value)) return new Date(value);
+  return value;
+}
 class IssueFilter extends React.Component {
   render() {
     return (
@@ -19,7 +11,6 @@ class IssueFilter extends React.Component {
     );  
   }
 }
-
 class IssueRow extends React.Component {  
   render() {
     const issue = this.props.issue;
@@ -30,7 +21,7 @@ class IssueRow extends React.Component {  
         <td>{issue.owner}</td>
         <td>{issue.created.toDateString()}</td>
         <td>{issue.effort}</td>
-        <td>{issue.due ? issue.due.toDateString() : ''}</td>
+        <td>{issue.due ? issue.due.toDateString() : ' '}</td>
       <td>{issue.title}</td>
       </tr>
     );
@@ -72,7 +63,8 @@ class IssueAdd extends React.Component {
     e.preventDefault();
     const form = document.forms.issueAdd;
     const issue = {
-      owner: form.owner.value, title: form.title.value, status: 'New',
+      owner: form.owner.value, title: form.title.value,
+      due: new Date(new Date().getTime() + 1000*60*60*24*10),
     }
     this.props.createIssue(issue);
     form.owner.value = ""; form.title.value = "";
@@ -87,6 +79,32 @@ class IssueAdd extends React.Component {
     );
   }
 }
+
+async function graphQLFetch(query, variables = {}) {
+  try {
+    const response = await fetch('/graphql', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json'},
+      body: JSON.stringify({ query, variables })
+    });
+    const body = await response.text();
+    const result = JSON.parse(body, jsonDateReviver);
+
+    if (result.errors) {
+      const error = result.errors[0];
+      if (error.extensions.code == 'BAD_USER_INPUT') {
+        const details = error.extensions.exception.errors.join('\n ');
+        alert(`${error.message}:\n ${details}`);
+      } else {
+        alert(`${error.extensions.code}: ${error.message}`);
+      }
+    }
+    return result.data;
+  } catch (e) {
+    alert(`Error in sending data to server: ${e.message}`);
+  }
+}
+
 class IssueList extends React.Component {
   constructor() {
     super();
@@ -96,18 +114,32 @@ class IssueList extends React.Component {
   componentDidMount() {
     this.loadData();
   }
-  loadData() {
-    setTimeout(() => {
-      this.setState({ issues: initialIssues});
-    }, 500);
+  async loadData() {
+    const query = `query {
+      issueList {
+        id title status owner
+        created effort due
+      }
+    }`;
+    const data = await graphQLFetch(query);
+    if (data) {
+      this.setState({ issues: data.issueList });
+    }
   }
-  createIssue(issue) {
-    issue.id = this.state.issues.length + 1;
-    issue.created = new Date();
-    const newIssueList = this.state.issues.slice();
-    newIssueList.push(issue);
-    this.setState({ issues: newIssueList });
+
+  async createIssue(issue) {
+    const query = `mutation issueAdd($issue: IssueInputs!) {
+      issueAdd(issue: $issue) {
+        id
+      }
+    }`;
+
+    const data = await graphQLFetch(query, { issue });
+    if (data) {
+      this.loadData();
+    }
   }
+
   render() {
     return (
       <React.Fragment>
